8a48b356982b8745e419fc4037a54ab287568886
becca3 <32517979+becca3@users.noreply.github.com>
2020-01-14 15:58:52 -0900

    Comments

--- Labs/lab2/lab2.h
+++ Labs/lab2/lab2.h
@@ -11,10 +11,10 @@ private:
 	int x, y;
 
 public:
-	//Default constructor.
+	//Default constructor. A constructor that can be called without parameters.
 	functions(int x1_, int y1_) { x = x1_; y = y1_; }
 
-	//Copy constructor.
+	//Copy constructor. A member function which initializes an object using another object of the same class.
 	functions(const functions& item) { x = item.x; y = item.y; }
 
 	int getX() { return x; }
