77aa87123751fd38c023c5d0e3749c004b0c497e
becca3 <32517979+becca3@users.noreply.github.com>
2020-01-15 15:53:54 -0900

    Default constructor and deconstructor

--- Labs/lab2/lab2.cpp
+++ Labs/lab2/lab2.cpp
@@ -2,10 +2,5 @@
 
 int main(int argc, char** argv)
 {
-	functions orig(5, 50); //Default constructor called. 
-	functions item = orig; //Copy constructor called. 
-
-	std::cout << "Default constructor: " << "item x = " << orig.getX() << ", item y = " << orig.getY() << "\n";
-	std::cout << "Copy constructor: " << "item x = " << item.getX() << ", item y = " << item.getY();
-
+	functions();
 }
--- Labs/lab2/lab2.h
+++ Labs/lab2/lab2.h
@@ -12,14 +12,19 @@ private:
 
 public:
 	//Default constructor. A constructor that can be called without parameters.
-	functions(int x1_, int y1_) { x = x1_; y = y1_; }
+	functions()
+	{
+		std::cout << "Default constructor called \n";
+	}
 
+	//Deconstructor.
+	~functions()
+	{
+		std::cout << "Deconstructor called\n";
+	}
 	//Copy constructor. A member function which initializes an object using another object of the same class.
-	functions(const functions& item) { x = item.x; y = item.y; }
-
-	int getX() { return x; }
-	int getY() { return y; }
 
 	//Move constructor. 
+		
 };
 #endif
