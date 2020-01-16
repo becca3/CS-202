1711f3233b81bf4e8c02e100b45a0ecd1131e22f
becca3 <32517979+becca3@users.noreply.github.com>
2020-01-15 16:08:42 -0900

    Copy constructor - need to work on additonal constructor

--- Labs/lab2/lab2.cpp
+++ Labs/lab2/lab2.cpp
@@ -2,5 +2,12 @@
 
 int main(int argc, char** argv)
 {
+	//Default and deconstructor.
 	functions();
+
+	//Copy constructor.
+	functions x2(5, 50);
+	functions x1 = x2;
+	std::cout << "x2.x = " << x2.geta() << ", x2.y = " << x2.getb() << "\n";
+	std::cout << "x1.x = " << x1.geta() << ", x1.y = " << x1.getb() << "\n";
 }
