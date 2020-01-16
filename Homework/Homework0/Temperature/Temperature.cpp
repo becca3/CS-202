01cf6d01559d5b6a25719a881957c0e41e9e7593
becca3 <32517979+becca3@users.noreply.github.com>
2020-01-15 16:41:18 -0900

    added main body

--- Homework/Homework0/Temperature/Temperature.cpp
+++ Homework/Homework0/Temperature/Temperature.cpp
@@ -1,6 +1,44 @@
 #include<iostream>
+#include<string>
 
+//Variables.
+std::string ans;
+const char* str;
+
+//Functions. 
+double cpp_ftoc(const char* str);
+double c_ctof(const char* str);
+
+//Main.
 int main(int argc, char** argv)
 {
+	std::cout << "1 - Celsius to fahrenheit or \n";
+	std::cout << "2 - Fahrenheit to celcius? \n";
+	std::cin >> ans;
+
+	if (ans == "1")
+	{
+		c_ctof(str);
+	}
+	else if (ans == "2")
+	{
+		cpp_ftoc(str);
+	}
+	else
+	{
+		std::cout << "invalid input \n";
+	}
 	return 0;
+}
+
+//C++ 
+double cpp_ftoc(const char* str)
+{
+
+}
+
+//C
+double c_ctof(const char* str)
+{
+
 }
