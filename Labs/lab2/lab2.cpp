453c412dca406be7b462aadd3b29a22a67a8f73d
becca3 <32517979+becca3@users.noreply.github.com>
2020-01-15 16:10:42 -0900

    Comments and changes (minor)

--- Labs/lab2/lab2.cpp
+++ Labs/lab2/lab2.cpp
@@ -8,6 +8,6 @@ int main(int argc, char** argv)
 	//Copy constructor.
 	functions x2(5, 50);
 	functions x1 = x2;
-	std::cout << "x2.x = " << x2.geta() << ", x2.y = " << x2.getb() << "\n";
-	std::cout << "x1.x = " << x1.geta() << ", x1.y = " << x1.getb() << "\n";
+	std::cout << "x2.a = " << x2.geta() << ", x2.b = " << x2.getb() << "\n";
+	std::cout << "x1.a = " << x1.geta() << ", x1.b = " << x1.getb() << "\n";
 }
