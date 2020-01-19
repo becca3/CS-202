961c96df131fdb9bf48c751a2868e37dfc9e7dd4
becca3 <32517979+becca3@users.noreply.github.com>
2020-01-19 11:13:06 -0900

    Major edits and attempt to fix it

---
+++ Homework/Homework0/Printer/Printer.cpp
@@ -0,0 +1,14 @@
+/*
+Program: Printer.
+Author: Rebecca Morgan 
+Date: 17th January 2020
+Description: program to print excerpt from book. 
+*/
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
--- Homework/Homework0/Temperature/Temperature.cpp
+++ Homework/Homework0/Temperature/Temperature.cpp
@@ -1,47 +1,74 @@
-/*
-Program: Homework 0 
-Author: Rebecca Morgan
-Date: 17th January 2020
-Description: Temperature conversion program. 
-*/
-///* argc = 3 */
+///*
+//Program: Homework 0 
+//Author: Rebecca Morgan
+//Date: 17th January 2020
+//Description: Temperature conversion program. 
+
+
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::stod;
+
+#include <cstdlib>
+using std::strtod;
 
 #include <iostream>
-#include <cstdlib>
-#include <string>
+using std::cout;
+using std::endl;
 
-double cpp_ftoc(const char* str);
-double c_ctof(const char* str);
+#include <stdexcept>
+using std::invalid_argument;
+
+//Takes the given C-string, cstr, which
+//represents a temperature in Celcius
+//and converts it to Fahrenheit using the 
+//strtod method in the header cstdlib.
+
+double c_ctof(const char* cstr)
+{
+	double return_value = 0.0;
+
+	return_value = strtod(cstr, nullptr);
+
+	//Modifies the double from celcius
+	//to fahrenheit.
+	return_value *= 9.0 / 5.0;
+	return_value += 32;
+
+	return return_value;
+}
+
+//Takes the given C-string, cstr, which
+//represents a temperature in Fahrenheit
+//and converts it to Celcius using the 
+//stod method in the header string.
+
+double cpp_ftoc(const char* cstr)
+{
+	double return_value = 0.0;
+
+	//Creates a temporary C++string that
+	//stod uses.
+	string str(cstr);
+	return_value = stod(str);
+
+	//Modifies the double from fahrenheit
+	//to celcius.
+	return_value -= 32;
+	return_value *= 5.0 / 9.0;
+
+	return return_value;
+}
 
 int main(int argc, const char** argv)
 {
-	if (argc >= 2 && argv[1] == std::string("--ftoc"))
-	{
-		cpp_ftoc(argv[2]);
-	}
-}
-
-double cpp_ftoc(const char* str)
-{
-	std::cout << "ftoc";
+	std::cout << "Temp \n";
+	c_ctof;
+	cpp_ftoc;
 	return 0;
 }
-
-double c_ctof(const char* str)
-{
-	std::cout << "ctof";
-	return 0;
-}
-
-
-
-
-
-
-
-
-
-
 
 
 
