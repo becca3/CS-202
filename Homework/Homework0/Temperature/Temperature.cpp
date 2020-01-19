963affc1f403fd69a95c637297f9fd61487bad2d
becca3 <32517979+becca3@users.noreply.github.com>
2020-01-19 11:18:21 -0900

    Finished program

--- Homework/Homework0/Temperature/Temperature.cpp
+++ Homework/Homework0/Temperature/Temperature.cpp
@@ -1,9 +1,8 @@
 ///*
 //Program: Homework 0 
 //Author: Rebecca Morgan
-//Date: 17th January 2020
+//Date: 19th January 2020
 //Description: Temperature conversion program. 
-
 
 #include <string>
 #include <iostream>
@@ -15,38 +14,32 @@
 //represents temperature in Celcius
 //converts it to Fahrenheit using the 
 //strtod method.
-
 double c_ctof(const char* str)
 {
-	double return_value = 0.0;
+	double return_val = 0.0;
+	return_val = strtod(str, nullptr);
 
-	return_value = strtod(str, nullptr);
+	//Modifies double from celcius to fahrenheit.
+	return_val *= 9.0 / 5.0;
+	return_val += 32;
 
-	//Modifies double from celcius
-	//to fahrenheit.
-	return_value *= 9.0 / 5.0;
-	return_value += 32;
-
-	return return_value;
+	return return_val;
 }
 
 //Takes C-string, str, which
 //represents temperature in Fahrenheit
 //and converts it to Celcius.
-
 double cpp_ftoc(const char* cstr)
 {
-	double return_value = 0.0;
-
+	double return_val = 0.0;
 	std::string str(cstr);
-	return_value = std::stod(str);
+	return_val = std::stod(str);
 
-	//Modifies the double from fahrenheit
-	//to celcius.
-	return_value -= 32;
-	return_value *= 5.0 / 9.0;
+	//Modifies the double from fahrenheit to celcius.
+	return_val -= 32;
+	return_val *= 5.0 / 9.0;
 
-	return return_value;
+	return return_val;
 }
 
 int main(int argc, const char** argv)
