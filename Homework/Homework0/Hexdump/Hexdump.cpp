cf7bf7774e9a69df6661febd326caf7dfcae0311
becca3 <32517979+becca3@users.noreply.github.com>
2020-01-19 13:47:36 -0900

    Broke it and started again

---
+++ Homework/Homework0/Hexdump/Hexdump.cpp
@@ -0,0 +1,19 @@
+/*
+Program: Homework 0 - Hexdump 
+Author: Rebecca Morgan
+Date: 19th January 2020
+Description: Program to dump contents of a file as hexadecimal characters. 
+*/
+
+# include <cstdlib>
+# include <iostream>
+# include <iomanip>
+# include <fstream>
+# include <ctime>
+
+void handle(char file_in_name[]);
+
+int main(long argc, char* argv[])
+{
+
+}
