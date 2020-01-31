0f8dda76041ae8d9ad1eb093012880599f4e94b7
becca3 <32517979+becca3@users.noreply.github.com>
2020-01-31 13:56:35 -0900

    Populated struct with values

--- Homework/Homework2/Linked list/Linked list/linked_list_main.cpp
+++ Homework/Homework2/Linked list/Linked list/linked_list_main.cpp
@@ -7,63 +7,28 @@
 //Struct of animals. 
 struct Animal
 {
-	int animal;  
-	std::unique_ptr<Animal> next;
-	Animal(int animal) : animal{animal}, next{ nullptr } {}
-
-	~Animal()
-	{
-		std::cout << "Destroy node with animal: " << animal << '\n';
-	}
+	std::string species;
+	std::string zoo;
+	std::string food;
+	int id;
+	int age;
+	int number;
 };
 
 //List struct to manage creation and deletion of nodes. 
-struct List
-{
-	List() : head{ nullptr } {};
-	void push(int animal) 
-	{
-		auto temp{ std::make_unique<Animal>(animal) };
-		if (head)
-		{
-			temp->next = std::move(head);
-			head = std::move(temp);
-		}
-		else
-		{
-			head = std::move(temp);
-		}
-	}
-	void pop() 
-	{
-	
-	}
-	~List() 
-	{
-	
-	}
-	
-private:
-	std::unique_ptr<Animal> head;
-};
 
 int main(int argc, char** argv)
 {
     //Create node n0.
-	auto n0{ std::make_unique<Animal>(0) };
 
 	//Create node n1.
-	auto n1{ std::make_unique<Animal>(1) };
 
 	//Link node n0 to n1. And so on..
-	n1->next = std::move(n0);
-	auto n2{ std::make_unique<Animal>(2) };
-	n2->next = std::move(n1);
+
 
 	//Create an object of List struct.
-	List test;
+	
 
 	//Pushing elements to the head of the list. 
-	test.push(5);
-	test.push(6);
+	
 }
