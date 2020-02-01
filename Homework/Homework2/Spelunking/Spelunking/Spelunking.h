3662b02e72a6835fdec5172c195a7c7ad868ea16
becca3 <32517979+becca3@users.noreply.github.com>
2020-01-31 20:06:46 -0900

    added map class to lay out map

--- Homework/Homework2/Spelunking/Spelunking/Spelunking.h
+++ Homework/Homework2/Spelunking/Spelunking/Spelunking.h
@@ -27,4 +27,30 @@ public:
     int room() const { return currRoom; }
     int getAdj(int i) const { return adjRooms[i]; }
 };
+
+class Map
+{
+    //Defines number of rooms present. 
+    std::vector<Room> cave{ std::vector<Room>(20) };
+    //Keeps track of empty rooms.
+    std::vector<int> vacant; 
+
+    Player play;
+
+    void addWumpus();
+    void addBats();
+    void addPits();
+    void addPlayer();
+
+    void reportState();
+    int input();
+
+    int movePlayer(int);
+    int shoot(int target);
+    void batEncounter();
+    int moveWumpus();
+public:
+    void init();
+    void play();
+};
 #endif // !SPELUNKING_H
