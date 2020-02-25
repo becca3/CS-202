d456fcbd617538b4eae7615b784c12fb17444c80
becca3 <32517979+becca3@users.noreply.github.com>
2020-02-25 12:45:22 -0900

    library needs to be included in the path now

---
+++ Labs/staticlibrary/OurLibrary/A.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include <iostream>
+
+void functionfromA()
+{
+	/*std::cout << "Hello World! \n";*/
+}
--- Labs/staticlibrary/staticlib_c++/Program1.cpp
+++ Labs/staticlibrary/staticlib_c++/Program1.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <A.h>
+
+#pragma comment(lib, OurLibrary.lib)
+
 
 int main(int argc, char** argv)
 {
+	functionfromA();
+	std::cout << "Hello World! \n";
 	return 0;
 }
