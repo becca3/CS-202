0a93f21b0708f976242e9ed4f67cabc47821eaa3
becca3 <32517979+becca3@users.noreply.github.com>
2020-02-27 11:08:18 -0900

    initial commit for static lib

---
+++ Labs/LabStaticLibrary/LabStaticLibrary/LabStaticLibrary/LabStaticLibrary.cpp
@@ -0,0 +1,10 @@
+// LabStaticLibrary.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnLabStaticLibrary()
+{
+}
