15fb9ffc52ea02592cbd2985fe7d084a5c5cdcb0
becca3 <32517979+becca3@users.noreply.github.com>
2020-03-28 20:19:32 +0000

    began creation of complex class

--- Labs/lab8/lab8/complex.h
+++ Labs/lab8/lab8/complex.h
@@ -1,6 +1,15 @@
 #ifndef COMPLEX_H
 #define COMPLEX_H
 
+class Complex
+{
+public:
+	Complex() {}
+	Complex(double real, double imag = 0);
 
+private:
+	double real_{ 0 };
+	double imag_{ 0 };
+};
 
 #endif 
--- Labs/lab8/lab8/complex_main.cpp
+++ Labs/lab8/lab8/complex_main.cpp
@@ -1,6 +1,10 @@
 #include "complex.h"
+#include <iostream>
+
 
 int main(int argc, char** argv)
 {
+	std::cout << Complex(2, 5) << "\n";
+	//std::cout << 1 + Complex(2, 5) << "\n";
 	return 0;
 }
