9e0a93e46990f4a44ad4ef85fae934ed5b98829b
becca3 <32517979+becca3@users.noreply.github.com>
2020-03-29 00:41:24 +0000

    Tested and it works

--- Homework/Homework4/factorial/factorial.cpp
+++ Homework/Homework4/factorial/factorial.cpp
@@ -17,16 +17,22 @@ int fact_loop(int n)
     int f = 1;
 
     for (int i = n; i > 1; --i)
+    {
         f *= i;
+    }
 
     return f;
 }
 
 int main()
 {
+    std::cout << "Recursive factorial: \n";
     std::cout << "5 = 5 * 4 * 3 * 2 * 1 = " << fact(5) << std::endl;
 
     std::cout << std::endl;
+
+    std::cout << "Non-recursive factorial: \n";
+    std::cout << "5 = 5 * 4 * 3 * 2 * 1 = " << fact_loop(5) << std::endl;
 
 	return 0; 
 }
