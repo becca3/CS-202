484342ea433738bc5aef78891dabcb4b2b75b79d
becca3 <32517979+becca3@users.noreply.github.com>
2020-03-29 22:08:21 +0100

    Created fibonacci - non-recursive

--- Iditarod/Challenge 1/Anchorage-recursion problems/fibonacci.cpp
+++ Iditarod/Challenge 1/Anchorage-recursion problems/fibonacci.cpp
@@ -24,3 +24,38 @@ int fib(int x)
 
     return f;
 }
+
+//Non-recursive fib
+int fib_loop(int n)
+{
+    static int f0 = 0;
+    static int f1 = 1;
+    static int f;
+
+    if (n == 0)
+        return 0;
+
+    if (n == 1)
+        return 1;
+
+    while (n > 0)
+    {
+        f = f0 + f1;
+
+        if (__debug__)
+            std::cout << f0 << " + " << f1 << " = " << f << std::endl;
+
+        f0 = f1;
+        f1 = f;
+
+        --n;
+    }
+
+    return f;
+}
+
+int main()
+{
+
+    return 0;
+}
