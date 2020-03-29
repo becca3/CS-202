783c8810c5e9e7790e42bcdf33fbd1aedd9efb69
becca3 <32517979+becca3@users.noreply.github.com>
2020-03-29 22:14:04 +0100

    created non-recursive factorial

--- Iditarod/Challenge 1/factorial/factorial.cpp
+++ Iditarod/Challenge 1/factorial/factorial.cpp
@@ -11,6 +11,19 @@ int fact(int n)
     return n * fact(n - 1);
 }
 
+//Non-recursive factorial
+int fact_loop(int n)
+{
+    int f = 1;
+
+    for (int i = n; i > 1; --i)
+    {
+        f *= i;
+    }
+
+    return f;
+}
+
 int main()
 {
 
