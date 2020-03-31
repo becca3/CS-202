f5caea9a23d6fa3f8afb81f66d1b25be0ca2bd09
becca3 <32517979+becca3@users.noreply.github.com>
2020-03-31 23:51:14 +0100

    created global static variables

--- Labs/lab9/lab9/lab9.cpp
+++ Labs/lab9/lab9/lab9.cpp
@@ -2,10 +2,39 @@
 #include <iomanip>
 #include <string>
 
+//Global static.
+static int ani_count = 0;	
+
+class Animal_stat
+{
+public:
+	Animal_stat() : animalNum_(ani_count)
+	{
+		std::cout << __FUNCTION__ << " " << animalNum_ << " initialised. \n";
+		ani_count++;
+	}
+
+	~Animal_stat()
+	{
+		std::cout << __FUNCTION__ << " " << animalNum_ << " destroyed.\n";
+	}
+
+private:
+	int animalNum_;
+};
+
+//Also global static.
+namespace
+{
+	Animal_stat bear1;
+	Animal_stat bear2;
+}
+
 void ani_static()
 {
 	//Static local variable. 
 	static int count = 0;
+	static Animal_stat bear; 
 	
 	if (!count)
 	{
@@ -14,15 +43,17 @@ void ani_static()
 	}
 	else
 	{
-		std::cout << __FUNCTION__ << " called " << count << " times \n";
+		std::cout << __FUNCTION__ << " called " << count << " times. \n";
 		count++;
 	}
 }
 
 int main(int argc, char** argv)
 {
+	std::cout << "Starting main\n";
 	ani_static();
 	ani_static();
 	ani_static();
+	std::cout << "Leaving main\n";
 	return 0;
 }
