e3f8a96dee8d8f6a48acf1515e2410b57d05547a
becca3 <32517979+becca3@users.noreply.github.com>
2020-03-31 23:31:21 +0100

    Created static local variable

--- Labs/lab9/lab9/lab9.cpp
+++ Labs/lab9/lab9/lab9.cpp
@@ -2,7 +2,27 @@
 #include <iomanip>
 #include <string>
 
+void ani_static()
+{
+	//Static local variable. 
+	static int count = 0;
+	
+	if (!count)
+	{
+		std::cout << __FUNCTION__ << " is not initialised.\n";
+		count = 1;
+	}
+	else
+	{
+		std::cout << __FUNCTION__ << " called " << count << " times \n";
+		count++;
+	}
+}
+
 int main(int argc, char** argv)
 {
+	ani_static();
+	ani_static();
+	ani_static();
 	return 0;
 }
