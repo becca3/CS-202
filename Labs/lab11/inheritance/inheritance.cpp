4ced80438f6cc3e275e7c86f0b1680fe5da0ffc0
becca3 <32517979+becca3@users.noreply.github.com>
2020-04-13 15:57:08 +0100

    Created a base class object using one parameter constructor

--- Labs/lab11/inheritance/inheritance.cpp
+++ Labs/lab11/inheritance/inheritance.cpp
@@ -7,16 +7,30 @@ class Base
 public:
 	Base()
 	{
-		std::cout << "Constructing the default base object. Initial value is " << age;
+		std::cout << "Constructing the default base object. Initial value is " << age_ << "\n";
+	}
+	
+	Base(int age) :age_(age)
+	{
+		std::cout << "Constructing a base object with value " << age_ << "\n";
+	}
+
+	~Base()
+	{
+		std::cout << "Destructing a base object with value " << age_ << "\n";
 	}
 
 private:
-	int age{ 22 };
+	int age_{ 22 };
 };
 
 int main(int argc, char** argv)
 {
 	{ Base a; }
 	std::cout << "\n";
+
+	{ Base a{ 20 }; }
+	std::cout << "\n";
+
 	return 0;
 }
