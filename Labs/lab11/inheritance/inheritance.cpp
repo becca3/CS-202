2adb04028fd0f05b089c2aca2df2f5b23c613136
becca3 <32517979+becca3@users.noreply.github.com>
2020-04-13 15:52:13 +0100

    created a default base class

--- Labs/lab11/inheritance/inheritance.cpp
+++ Labs/lab11/inheritance/inheritance.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 
+//Base class.
 
+class Base
+{
+public:
+	Base()
+	{
+		std::cout << "Constructing the default base object. Initial value is " << age;
+	}
+
+private:
+	int age{ 22 };
+};
 
 int main(int argc, char** argv)
 {
-
+	{ Base a; }
+	std::cout << "\n";
 	return 0;
 }
