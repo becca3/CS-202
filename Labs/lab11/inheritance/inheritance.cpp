201be2d33fc281f1caac5bdf11e791e02809506b
becca3 <32517979+becca3@users.noreply.github.com>
2020-04-13 16:02:20 +0100

    created a default derived class object

--- Labs/lab11/inheritance/inheritance.cpp
+++ Labs/lab11/inheritance/inheritance.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 
 //Base class.
 
@@ -24,12 +25,35 @@ private:
 	int age_{ 22 };
 };
 
+
+//Derived class 
+
+class Derived : public Base
+{
+public:
+	Derived() : Base(50), name_{ "Andrea" }
+	{
+		std::cout << "Constructing derived object, name is " << name_ << "\n";
+	}
+
+	~Derived()
+	{
+		std::cout << "Deconstructing the derived object with name " << name_ << "\n";
+	}
+
+private:
+	std::string name_;
+};
+
 int main(int argc, char** argv)
 {
 	{ Base a; }
 	std::cout << "\n";
 
 	{ Base a{ 20 }; }
+	std::cout << "\n";
+
+	{ Derived b; }
 	std::cout << "\n";
 
 	return 0;
