b39e014faaa9ea2682bd940fd396d8bcef944320
becca3 <32517979+becca3@users.noreply.github.com>
2020-04-21 18:33:01 +0100

    write function template twice and test with int and float

--- Labs/lab14/lab14/lab14.cpp
+++ Labs/lab14/lab14/lab14.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+template <typename T> 
+T twice(const T& x)
+{
+	return x + x;
+}
+
 int main(int argc, char** argv)
 {
-
+	std::cout << twice(2) << std::endl;
+	std::cout << twice(2.3) << std::endl;
 	return 0;
 }
