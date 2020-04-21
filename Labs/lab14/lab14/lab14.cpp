d98459190c74f9510c22c6b03042afd9a839007d
becca3 <32517979+becca3@users.noreply.github.com>
2020-04-21 18:38:14 +0100

    fix it to work with a C style string

--- Labs/lab14/lab14/lab14.cpp
+++ Labs/lab14/lab14/lab14.cpp
@@ -6,10 +6,16 @@ T twice(const T& x)
 	return x + x;
 }
 
+std::string twice(const char* x)
+{
+	return twice(std::string(x));
+}
+
 int main(int argc, char** argv)
 {
 	std::cout << twice(2) << std::endl;
 	std::cout << twice(2.3) << std::endl;
-	std::cout << twice(std::string("Hello"));
+	std::cout << twice(std::string("Hello")) << std::endl;
+	std::cout << twice("Hello") << std::endl;
 	return 0;
 }
